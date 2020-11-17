33c6f53c697a5179e6310eb453e4b6298c721464
John A. De Goes <john@degoes.net>
2020-11-17 17:03:12 -0500

    More exercises & welcome

--- src/main/scala/fdm/07-best-practices.scala
+++ src/main/scala/fdm/07-best-practices.scala
@@ -103,6 +103,77 @@ object extract_product {
 }
 
 /**
+ * Another anti-pattern occurs when many fields of a case class are optional (or `null`). This
+ * indicates there is a "missing enumeration" that can be extracted from the case class, which
+ * bundles fields that always appear together.
+ *
+ * In this case, the "extract sum" refactoring involves identifying patterns of optional fields
+ * that always appear together, and pulling them out into the cases of a new enumeration.
+ */
+object extract_sum {
+  final case class Job(title: String, salary: Double)
+
+  final case class Enrollment(university: String, credits: Int, year: java.time.YearMonth)
+
+  /**
+   * EXERCISE 1
+   *
+   * Extract out a missing enumeration from the following data type.
+   */
+  final case class Person(
+    name: String,
+    job: Option[Job],                                // employed
+    employmentDate: Option[java.time.LocalDateTime], // if employed
+    school: Option[Enrollment]                       // full-time student
+  )
+
+  /**
+   * EXERCISE 2
+   *
+   * Extract out a missing enumeration from the following data type.
+   */
+  final case class Event(
+    deviceId: Option[String], // sensor initiated event
+    userId: Option[String],   // user initiated event
+    timestamp: java.time.Instant,
+    reading: Option[Double], // for sensors
+    click: Option[String],   // for user click events
+    purchase: Option[String] // for user order events
+  )
+
+  /**
+   * EXERCISE 3
+   *
+   * Extract out a missing enumeration from the following data type.
+   */
+  final case class CreditCard(
+    digit16: Option[Digit16],    // For VISA
+    digit15: Option[Digit15],    // For AMEX
+    securityCode: Option[Digit4] // For VISA
+  )
+
+  final case class Digit16(group1: Digit4, group2: Digit4, group3: Digit4, group4: Digit4)
+
+  final case class Digit15(group1: Digit4, group2: Digit4, group3: Digit4, group4: Digit3)
+
+  final case class Digit4(v1: Digit, v2: Digit, v3: Digit, v4: Digit)
+  final case class Digit3(v1: Digit, v2: Digit, v3: Digit)
+
+  sealed trait Digit {
+    case object _0 extends Digit
+    case object _1 extends Digit
+    case object _2 extends Digit
+    case object _3 extends Digit
+    case object _4 extends Digit
+    case object _5 extends Digit
+    case object _6 extends Digit
+    case object _7 extends Digit
+    case object _8 extends Digit
+    case object _9 extends Digit
+  }
+}
+
+/**
  * Another anti-pattern is when a code base has (unsealed) traits that are used for storing data.
  * Such code that encourage runtime type checking using `isInstanceOf` or equivalent, which is
  * neither safe, nor easy to maintain as new subtypes are introduced into the code base.
@@ -130,4 +201,120 @@ object seal_data_trait {
   trait GroupOwnedDocument extends Document {
     def ownerIds: List[String]
   }
+}
+
+/**
+ * Wildcard pattern matches on enumerations that change frequently are an anti-pattern, because
+ * when new cases are added, there is no way for the compiler to remind developers to update
+ * business logic involving the enumeration, which leads to subtle bugs in application logic.
+ *
+ * In this case, we can apply a "eliminate wildcard" refactoring that involves explicitly matching
+ * against all cases of an enumeration.
+ */
+object eliminate_wildcard {
+  trait Phone
+  def sendText(phone: Phone, message: String): Unit = println(s"Sending a text to ${phone}: ${message}")
+
+  /**
+   * EXERCISE 1
+   *
+   * Eliminate the wild card pattern match, and instead move to matching each case individually.
+   */
+  def pageDeveloper(event: Event, onCall: Phone): Unit =
+    event match {
+      case Event.ServerDown => sendText(onCall, "The server is down, please look into it right away!")
+      case _                =>
+    }
+
+  /**
+   * EXERCISE 2
+   *
+   * Add another type of event, such as `BillingOverage`, which might require immediate attention
+   * from a devops engineer. The Scala compiler will make you update existing code safely.
+   */
+  sealed trait Event
+  object Event {
+    case object ServerDown       extends Event
+    case object ServiceRestarted extends Event
+  }
+}
+
+/**
+ * Another anti-pattern is when code uses pattern matching to perform a so-called "type case".
+ * Type cases are when the runtime type of a value is checked, rather than deconstructing the
+ * value using one of the cases of the enumeration. Matching cases of an enumeration helps ensure
+ * that no implementation details leak into pattern matches, discourages using traits in any other
+ * way than for enumerations, and helps give developers a chance to update logic when new
+ * fields are added to case classes.
+ */
+object eliminate_typecase {
+  sealed trait Event
+  sealed trait IdentifiedEvent extends Event {
+    def id: String
+  }
+  final case class Click(href: String)                extends Event
+  final case class Purchase(id: String, item: String) extends IdentifiedEvent
+
+  /**
+   * EXERCISE 1
+   *
+   * Refactor this code to eliminate the type case. You may have to refactor the data model too.
+   */
+  def logIdentifiedEvents(event: Event): Unit =
+    event match {
+      case identified: IdentifiedEvent => println("User event: " + identified.id)
+      case _                           =>
+    }
+
+}
+
+/**
+ * Another anti-pattern is creating many variables of the same type in a given scope by pattern
+ * matching on recursive data structures and giving values of the same type different names.
+ *
+ * In this case, we can apply the "shadow variable" refactoring and deliberately choose to
+ * shadow variables of the same name and type in outer scopes, reducing the possibility that we
+ * accidentally refer to a variable in an outer scope.
+ */
+object nested_shadowing {
+
+  /**
+   * EXERCISE 1
+   *
+   * Identify the bug caused by too many variables of the same type in the same scope, and fix it
+   * by applying the shadow variable refactoring.
+   */
+  def count[A](list: List[A]): Int =
+    list match {
+      case Nil       => 0
+      case _ :: tail => 1 + count(list)
+    }
+
+  sealed trait UserBehavior
+  object UserBehavior {
+    case object Purchase                                                 extends UserBehavior
+    case object Return                                                   extends UserBehavior
+    case object Anything                                                 extends UserBehavior
+    final case class Sequence(first: UserBehavior, second: UserBehavior) extends UserBehavior
+    final case class Not(behavior: UserBehavior)                         extends UserBehavior
+  }
+
+  import UserBehavior._
+
+  /**
+   * EXERCISE 2
+   *
+   * Identify the bug caused by too many variables of the same type in the same scope, and fix it
+   * by applying the shadow variable refactoring.
+   */
+  def analyzePattern(b: UserBehavior): Boolean =
+    b match {
+      case Purchase => true
+      case Return   => true
+      case Anything => false
+      case Sequence(first, second) =>
+        analyzePattern(first) || analyzePattern(second)
+      case Not(value) =>
+        analyzePattern(b)
+    }
 }
