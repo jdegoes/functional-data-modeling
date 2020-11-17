1e794600712e1d1ae761920a2442608df17bbb3d
John A. De Goes <john@degoes.net>
2020-11-17 00:47:45 -0500

    Add welcome

---
+++ src/main/scala/fdm/welcome.scala
@@ -0,0 +1,19 @@
+package fdm
+
+object welcome {
+  //
+  // Please join and say hello:
+  //
+  // CHAT ROOM: https://discord.gg/dAbfksd
+  //
+  // Please git clone and build:
+  //
+  // REPOSITORY: https://github.com/jdegoes/functional-data-modeling
+  //
+  // Daily Schedule:
+  //
+  //    START  : 11:00 AM London Time
+  //    BREAK  :  2:00 PM London Time
+  //    RESUME :  3:00 PM London Time
+  //    END    :  7:00 PM London Time
+}
