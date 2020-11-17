8679731854169bc5ea0e912d76e24e1ab9bc9752
John A. De Goes <john@degoes.net>
2020-11-17 00:45:22 -0500

    Add application section

---
+++ src/main/scala/fdm/11-apps.scala
@@ -0,0 +1,28 @@
+package fdm
+
+/**
+ * PROJECT 1
+ *
+ * You are building a UI application. Create data models for UI events, data models for widgets,
+ * and data models for typical business entities such as users, accounts, profiles, and other
+ * related entities.
+ */
+object ui {}
+
+/**
+ * PROJECT 2
+ *
+ * You are building an investment application, which allows users to hold a portfolio of stocks and
+ * make buy/sell trades on a platform. Create data models for the user, the user's portfolio,
+ * symbols on stock exchanges, trade types, and other related entities.
+ */
+object investing {}
+
+/**
+ * PROJECT 3
+ *
+ * You are building a private social network platform. Create data models for users, posts, likes
+ * shares, user feeds, and so forth. Assume that posts may contain text, videos, links, images, and
+ * combinations of the above.
+ */
+object social_network {}
